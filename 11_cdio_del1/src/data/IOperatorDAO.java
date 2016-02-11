2cd4fefacfa13833f1a6b378371cacd6cc346880
Casper <kiwicasper@hotmail.com>
2016-02-11 15:05:50 +0100

    Data interface på plads

--- 11_cdio_del1/src/data/IOperatorDAO.java
+++ 11_cdio_del1/src/data/IOperatorDAO.java
@@ -1,5 +1,12 @@
 package data;
 
+import java.util.List;
+
 public interface IOperatorDAO {
+	OperatorDTO getOperatoer(int oprId) throws DALException;
+	List<OperatorDTO> getOperatoerList() throws DALException;
+	void createOperatoer(OperatorDTO opr) throws DALException;
+	void updateOperatoer(OperatorDTO opr) throws DALException;
+	void deleteOperatoer(OperatorDTO opr) throws DALException;
 
 }
--- 11_cdio_del1/src/data/OperatorDTO.java
+++ 11_cdio_del1/src/data/OperatorDTO.java
@@ -1,5 +1,37 @@
 package data;
 
-public class OperatorDTO {
+import java.util.List;
+
+public class OperatorDTO implements IOperatorDAO{
+
+	@Override
+	public OperatorDTO getOperatoer(int oprId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OperatorDTO> getOperatoerList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createOperatoer(OperatorDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateOperatoer(OperatorDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteOperatoer(OperatorDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
