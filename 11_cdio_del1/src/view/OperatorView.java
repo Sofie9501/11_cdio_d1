c9698bbea194ee6554c6821bdb3c830595a41261
Casper <kiwicasper@hotmail.com>
2016-02-11 15:13:34 +0100

    Startet på view

--- 11_cdio_del1/src/view/OperatorView.java
+++ 11_cdio_del1/src/view/OperatorView.java
@@ -1,5 +1,21 @@
 package view;
 
 public class OperatorView {
-
-}
+	
+	private void adminMenu(){
+		
+	}
+	
+	private void changePassword(){
+		
+	}
+	private void weighing(){
+		
+	}
+	private void exit(){
+		return;
+	}
+	private void addOperator(){
+		
+	}
+}	
