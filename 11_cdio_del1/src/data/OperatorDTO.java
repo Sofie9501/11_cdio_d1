52a9f63f6194c6169ca7a7a69232b82c7c844f17
Morten Due Christiansen <mdue84@gmail.com>
2016-02-13 20:09:19 +0100

    færdig implementering af OperatorDTO klassen

--- 11_cdio_del1/src/data/OperatorDTO.java
+++ 11_cdio_del1/src/data/OperatorDTO.java
@@ -1,8 +1,39 @@
 package data;
 
-import java.util.List;
-
+/**
+ *	OperatorDTO is the domain model for
+ *	operators in the system
+ */
 public class OperatorDTO{
-
+	private int oprId;
+	private String oprName;
+	private String cpr;			// CPR should be on the from xxxxxx-xxxx
+	private String password;
+	
+	
+	public int getOprId() {
+		return oprId;
+	}
+	public void setOprId(int oprId) {
+		this.oprId = oprId;
+	}
+	public String getOprName() {
+		return oprName;
+	}
+	public void setOprName(String oprName) {
+		this.oprName = oprName;
+	}
+	public String getCpr() {
+		return cpr;
+	}
+	public void setCpr(String cpr) {
+		this.cpr = cpr;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
