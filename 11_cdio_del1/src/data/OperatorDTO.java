759ef1bdac9d4683e9738fedcd62ceb03f6acfe7
Morten Due Christiansen <mdue84@gmail.com>
2016-02-13 16:55:57 +0100

    ændret så OperatorData implementer IOperatorDAO istedet for OperatorDTO

--- 11_cdio_del1/src/data/OperatorDTO.java
+++ 11_cdio_del1/src/data/OperatorDTO.java
@@ -2,36 +2,7 @@ package data;
 
 import java.util.List;
 
-public class OperatorDTO implements IOperatorDAO{
+public class OperatorDTO{
 
-	@Override
-	public OperatorDTO getOperatoer(int oprId) throws DALException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<OperatorDTO> getOperatoerList() throws DALException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void createOperatoer(OperatorDTO opr) throws DALException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updateOperatoer(OperatorDTO opr) throws DALException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteOperatoer(OperatorDTO opr) throws DALException {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
--- 11_cdio_del1/src/data/OperatorData.java
+++ 11_cdio_del1/src/data/OperatorData.java
@@ -1,5 +1,37 @@
 package data;
 
-public class OperatorData {
+import java.util.List;
+
+public class OperatorData implements IOperatorDAO{
+
+	@Override
+	public OperatorDTO getOperatoer(int oprId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OperatorDTO> getOperatoerList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createOperatoer(OperatorDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateOperatoer(OperatorDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteOperatoer(OperatorDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
