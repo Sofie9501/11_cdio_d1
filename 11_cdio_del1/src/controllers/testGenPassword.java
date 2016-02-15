d638bc006c9a91cbecd84e9e583015570e14c8d8
Niels Ulrich Matthiessen <Niels_ulrich@hotmail.com>
2016-02-15 15:49:04 +0100

    Ændringer i testgenpas

--- 11_cdio_del1/src/controllers/testGenPassword.java
+++ 11_cdio_del1/src/controllers/testGenPassword.java
@@ -6,12 +6,11 @@ public class testGenPassword {
 
 	public static void main(String[] args) {
 		
-		int passwordLength = 12;
+		int passwordLength = 8;
 		
 		for(int i=0; i<10;i++){
 			System.out.println(genPassword(passwordLength));
 		}
-
 	}
 
 	private static String genPassword(int passLength) {
@@ -22,37 +21,30 @@ public class testGenPassword {
 		int countTotal = 0;
 		
 		while(countTotal < passLength){
-			ascii = randInt();
-			if(((ascii >= 33 && ascii <= 47) || (ascii >= 58 && ascii <= 64) || (ascii >= 91 && ascii <= 96) || (ascii >= 123 && ascii <= 126)) && counter1 < 3){
+			ascii = randInt(33, 126);
+			if(((ascii >= 33 && ascii <= 47) || (ascii >= 58 && ascii <= 64) || (ascii >= 91 && ascii <= 96) || (ascii >= 123 && ascii <= 126)) && counter1 < 2){
 				password = ((char)ascii) + password;
 				counter1++;
 			}
-			if(ascii >= 48 && ascii <= 57 && counter2 < 3){
+			if(ascii >= 48 && ascii <= 57 && counter2 < 2){
 				password = ((char)ascii) + password;
 				counter2++;
 			}
-			if(ascii >= 65 && ascii <= 90 && counter3 < 3){
+			if(ascii >= 65 && ascii <= 90 && counter3 < 2){
 				password = ((char)ascii) + password;
 				counter3++;
 			}
-			if(ascii >= 97 && ascii <= 122 && counter4 < 3){
+			if(ascii >= 97 && ascii <= 122 && counter4 < 2){
 				password = ((char)ascii) + password;
 				counter4++;
 			}
-		//	System.out.println(countTotal);
 			countTotal = counter1 + counter2 + counter3 + counter4; 
 		}
-				
 		return password;
 	}
 	
-	private static int randInt() {
-
-		int randomNum;
-		
-		randomNum = (int) Math.round((Math.random()*1000));
-
-	    return randomNum;
+	private static int randInt(int min, int max) {
+	    return ((int)(Math.random()*(max-min)+min));
 	}
 
 }
