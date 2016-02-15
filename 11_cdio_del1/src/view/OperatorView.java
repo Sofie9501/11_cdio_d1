d50010912a8a9d300f91ba8d0c306ecdbf8f5ec7
Brian Christensen <Allegodenavnevartaget@hotmail.com>
2016-02-15 12:31:57 +0100

    I view er weighing lavet.

--- 11_cdio_del1/src/view/OperatorView.java
+++ 11_cdio_del1/src/view/OperatorView.java
@@ -1,6 +1,15 @@
 package view;
 
+import java.util.Scanner;
+
+import controllers.IOperatorCO;
+import controllers.OperatorCO;
+
 public class OperatorView {
+	IOperatorCO con = new OperatorCO();
+	public OperatorView(IOperatorCO con){
+		this.con = con;
+	}
 	
 	private void adminMenu(){
 		
@@ -10,6 +19,14 @@ public class OperatorView {
 		
 	}
 	private void weighing(){
+		double TARA = 0;
+		double brutto = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter TARA in kg." );
+		TARA = scan.nextDouble();
+		System.out.println("Enter gross in kg.");
+		brutto = scan.nextDouble();
+		System.out.println("The net is " + con.weighing(TARA, brutto));
 		
 	}
 	private void exit(){
