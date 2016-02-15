a1fa33c5bda461d19ce0e9d750ebce4d25f80851
Cecilie <cille112@yahoo.dk>
2016-02-15 15:50:18 +0100

    rettet view til

--- 11_cdio_del1/src/view/IOperatorView.java
+++ 11_cdio_del1/src/view/IOperatorView.java
@@ -4,15 +4,15 @@ public interface IOperatorView {
 
 	public int menuChoice();
 	public int adminMenuChoice();
-	public int getOperatorID();
+	public int getOprID();
 	public String getPassword();
 	public String getNewPassword();
 	public double getTara();
 	public double getBrutto();
-	public void showNetto();
-	public String getOperatorName();
+	public void showNetto(int Tara, int Brutto, int Netto);
+	public String getOprName();
 	public String getCPR();
-	public void showOperator (int oprId, String cpr, String name, String password);
+	public void showOpr (int oprId, String cpr, String name, String password);
 	public void showError (String error);
 	public int getUpdateChoice();
 }
