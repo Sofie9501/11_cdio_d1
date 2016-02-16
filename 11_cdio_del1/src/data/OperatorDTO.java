e0322bbef3ae89b278d084b72a6bb7e9870a6134
Morten Due Christiansen <mdue84@gmail.com>
2016-02-16 13:17:14 +0100

    tilføjet getters og setters til admin

--- 11_cdio_del1/src/data/OperatorDTO.java
+++ 11_cdio_del1/src/data/OperatorDTO.java
@@ -49,5 +49,15 @@ public class OperatorDTO{
 		this.password = password;
 	}
 
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	
+	
+
 
 }
